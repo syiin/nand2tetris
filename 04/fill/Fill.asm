ce448d57fe8521085110498e85c8525097eed4d8
eigenstir <xbio.oo@gmail.com>
2019-02-15 22:55:03 +0800

    Finished Fill.asm

--- 04/fill/Fill.asm
+++ 04/fill/Fill.asm
@@ -12,3 +12,77 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+
+  //32 16-bit words x 256 height
+
+
+(KEYOFF)
+//reset the counter and cursor
+  @8192
+  D = A
+  @i
+  M = D
+  @SCREEN
+  D=A
+  @cursor
+  M=D
+(WHT)
+//*cursor = 0
+  @cursor
+  A = M
+  M = 0
+//cursor = cursor + 1
+  @cursor
+  D = M
+  @1
+  D = D + A
+  @cursor
+  M = D
+//i--
+  @i
+  MD = M - 1
+//if (i > 0)
+  @WHT
+  D; JGT
+
+ //listener
+  @KBD
+  D = M
+  @KEYPRESS
+  D; JNE
+  @KEYOFF
+  D; JEQ
+
+
+(KEYPRESS)
+//reset the counter & cursor
+  @8192
+  D = A
+  @i
+  M = D
+  @SCREEN
+  D=A
+  @cursor
+  M=D
+(BLK)
+  @cursor
+  A = M
+  M = -1
+  @cursor
+  D = M
+  @1
+  D = D + A
+  @cursor
+  M = D
+  @i
+  MD = M - 1
+  @BLK
+  D; JGT
+
+  //listener
+  @KBD
+  D = M
+  @KEYPRESS
+  D; JNE
+  @KEYOFF
+  D; JEQ
