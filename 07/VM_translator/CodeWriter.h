137cd945d1a86d2a202b716d66863d261feb972e
syiin <xbio.oo@gmail.com>
2019-03-11 22:06:03 +0800

    finishing 1

--- 07/VM_translator/CodeWriter.h
+++ 07/VM_translator/CodeWriter.h
@@ -10,6 +10,7 @@ class CodeWriter
 
 private:
   std::map<std::string, std::string> arithTable;
+  std::map<std::string, std::string> memNameTable;
   std::string fileName;
   std::ofstream myfile;
 
@@ -32,42 +33,83 @@ public:
 
   std::string writePushPop(std::string theCommand, std::string segment, int index)
   {
+
     if (theCommand == "C_POP")
     {
-      myfile << "\n@" + std::to_string(index) + "\nD=A\n@" + segment + "\nD=D+A\n@temp\nM=D\n@SP\nM=M-1\nD=M\n@temp\nA=M\nM=D\n";
-      return "@" + std::to_string(index) + "\nD=A\n@" + segment + "\nD=D+A\n@temp\nM=D\n@SP\nM=M-1\nD=M\n@temp\nA=M\nM=D\n";
+      myfile << popSegToD(index, segment);
+      myfile << "\n@R13\nM=D\n";
+      myfile << popStackToD();
+      myfile << "@R13\nA=M\nM=D\n";
+      myfile << decrementStackPointer();
+      return popSegToD(index, segment) + "\n@R13\nM=D\n@SP\nA=M-1\nD=M\n@R13\nA=M\nM=D\n" + decrementStackPointer();
     }
     else if (theCommand == "C_PUSH")
     {
       if (segment == "constant")
       {
         myfile << "\n@" + std::to_string(index) + "\nD=A\n@SP\nM=M+1\nA=M-1\nM=D\n";
-        return "@" + std::to_string(index) + "\nD=A\n@SP\nM=M+1\nA=M-1\nM=D\n";
+        return "\n@" + std::to_string(index) + "\nD=A\n@SP\nM=M+1\nA=M-1\nM=D\n";
       }
       else
       {
-        myfile << "\n@" + std::to_string(index) + "\nD=A\n@" + segment + "\nD=D+A\nA=D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
-        return "@" + std::to_string(index) + "D=A\n@" + segment + "D=D+A\nA=D\nD=M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+        myfile << popSegToD(index, segment) + "\nA=D\nD=M\n@SP\nA=M\nM=D\n" + incrementStackPointer();
+        return popSegToD(index, segment) + "A=D\nD=M\n@SP\nA=M\nM=D\n" + incrementStackPointer();
       }
     }
   };
 
+  std::string popStackToD()
+  {
+    return "@SP\nM=M-1\nA=M\nD=M\n";
+  };
+
+  std::string popSegToD(int idx, std::string segment)
+  {
+    return "\n@" + std::to_string(idx) + "\nD=A\n@" + memNameTable[segment] + "\nD=D+A\n";
+  }
+
+  std::string decrementStackPointer()
+  {
+    return "\n@SP\nM=M-1\n";
+  }
+
+  std::string incrementStackPointer()
+  {
+    return "\n@SP\nM=M+1\n";
+  }
+
   void close()
   {
+    myfile << "(INFINITE_LOOP)\n@INFINITE_LOOP\n0;JMP\n";
     myfile.close();
   }
 
   void initTables()
   {
-    arithTable["add"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D+M\n@SP\nM=M-1\nA=M-1\nM=D";
-    arithTable["sub"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D-M\n@SP\nM=M-1\nA=M-1\nM=D";
-    arithTable["neg"] = "@SP\nA=M-1\nM=-M";
-    arithTable["eq"] = "@SP\nA=M-1\nD=M\n@0\nD=D&A\n@SP\nA=M-1\nM=D";
-    arithTable["gt"] = "";
-    arithTable["lt"] = "";
-    arithTable["and"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D|M\n@SP\nM=M-1\nA=M-1\nM=D";
-    arithTable["or"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D&M\n@SP\nM=M-1\nA=M-1\nM=D";
-    arithTable["not"] = "@SP\nA=M-1\nA=A-1\nD=!M\n@SP\nA=M\nM=D\n@SP\nM=M+1";
+    std::string decrementSP = "@SP\nM=M-1\nA=M-1\nM=D\n";
+    arithTable["add"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D+M\n" + decrementSP;
+    arithTable["sub"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D-M\n" + decrementSP;
+    arithTable["neg"] = "@SP\nA=M-1\nM=-M\n";
+
+    std::string failCase = "@SP\nA=M-1\nA=A-1\nM=0\n@CONTINUE\n0;JMP\n";
+    std::string successCase = "(SUCCESS)\n@SP\nA=M-1\nA=A-1\nM=1\n";
+
+    arithTable["eq"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=M-D\n@SUCCESS\nD; JEQ\n" + failCase + successCase + "(CONTINUE)\n" + decrementSP;
+    arithTable["gt"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=M-D\n@SUCCESS\nD; JGT\n" + failCase + successCase + "(CONTINUE)\n" + decrementSP;
+
+    arithTable["lt"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D-M\nD; JLT\n";
+
+    arithTable["and"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D|M\n" + decrementSP;
+    arithTable["or"] = "@SP\nA=M-1\nD=M\nA=A-1\nD=D&M\n" + decrementSP;
+    arithTable["not"] = "@SP\nA=M-1\nA=A-1\nD=!M\n@SP\nA=M\nM=D\n@SP\nM=M+1\n";
+
+    memNameTable["local"] = "LCL";
+    memNameTable["argument"] = "ARG";
+    memNameTable["this"] = "THIS";
+    memNameTable["that"] = "THAT";
+    memNameTable["pointer"] = "3";
+    memNameTable["temp"] = "5";
+    memNameTable["static"] = "16";
   }
 };
 
--- 07/VM_translator/main.cpp
+++ 07/VM_translator/main.cpp
@@ -39,4 +39,5 @@ int main(int argc, char *argv[])
     i++;
     myParser.advance();
   };
-}
+  myWriter.close();
+};
