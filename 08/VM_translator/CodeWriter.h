97d7e4dc4de0cef73cb21423a1a783a02671b855
syiin <xbio.oo@gmail.com>
2019-03-12 23:23:14 +0800

    Added VM Translator to 08

---
+++ 08/VM_translator/CodeWriter.h
@@ -0,0 +1,249 @@
+#ifndef CODE_H
+#define CODE_H
+
+#include <fstream>
+#include <map>
+#include <string>
+
+class CodeWriter
+{
+
+private:
+  std::map<std::string, std::string> arithTable;
+  std::map<std::string, std::string> memNameTable;
+  std::string fileName;
+  std::ofstream myfile;
+  int lineIdx = 0;
+
+public:
+  CodeWriter(std::string outputName)
+  {
+    fileName = outputName;
+    myfile.open(fileName);
+    initTables();
+  };
+
+  std::string writeArithmetic(std::string theCommand)
+  {
+    myfile << makeLogicString(theCommand);
+    return makeLogicString(theCommand);
+  };
+
+  std::string writePushPop(std::string theCommand, std::string segment, int index)
+  {
+    std::string outputString = "";
+    if (theCommand == "C_POP")
+    {
+      myfile << popSegToD(index, segment)
+             << "\n@R13\nM=D\n"
+             << popStackToD()
+             << "@R13\nA=M\nM=D\n";
+    }
+    else if (theCommand == "C_PUSH")
+    {
+      if (segment == "constant")
+      {
+        myfile << "\n@" + std::to_string(index)
+               << "\nD=A\n"
+               << pushDToStack();
+      }
+      else
+      {
+        myfile << popSegToD(index, segment)
+               << "\nA=D\nD=M\n@SP\nA=M\nM=D\n"
+               << incrementStackPointer();
+      }
+    }
+  };
+
+  std::string makeLogicString(std::string theCommand)
+  {
+    lineIdx++;
+    std::string lineString = "line" + std::to_string(lineIdx);
+    std::string failCase = "@SP\nA=M-1\nM=0\n@CONTINUE." + lineString + "\n0;JMP\n";
+    std::string successCase = "(SUCCESS." + lineString + ")\n@SP\nA=M-1\nM=-1\n";
+
+    if (theCommand == "add")
+    {
+      return popStackToD() + "\n@SP\nM=M-1\nA=M\nD=M+D\n" + pushDToStack();
+    }
+    else if (theCommand == "sub")
+    {
+      return popStackToD() + "\n@SP\nM=M-1\nA=M\nD=M-D\n" + pushDToStack();
+    }
+    else if (theCommand == "not")
+    {
+      return "@SP\nA=M-1\nM=!M\n";
+    }
+    else if (theCommand == "neg")
+    {
+      return "@SP\nA=M-1\nM=-M\n";
+    }
+    else if (theCommand == "eq")
+    {
+      return popStackToD() + "@SP\nA=M-1\nD=M-D\n@SUCCESS." + lineString + "\nD; JEQ\n" + failCase + successCase + "(CONTINUE." + lineString + ")\n";
+    }
+    else if (theCommand == "gt")
+    {
+      return popStackToD() + "@SP\nA=M-1\nD=M-D\n@SUCCESS." + lineString + "\nD; JGT\n" + failCase + successCase + "(CONTINUE." + lineString + ")\n";
+    }
+    else if (theCommand == "lt")
+    {
+      return popStackToD() + "@SP\nA=M-1\nD=M-D\n@SUCCESS." + lineString + "\nD; JLT\n" + failCase + successCase + "(CONTINUE." + lineString + ")\n";
+    }
+    else if (theCommand == "and")
+    {
+      return popStackToD() + "\n@SP\nA=M-1\nD=D&M\nM=D\n";
+    }
+    else if (theCommand == "or")
+    {
+      return popStackToD() + "\n@SP\nA=M-1\nD=D|M\nM=D\n";
+    };
+  }
+
+  std::string popStackToD()
+  {
+    return "@SP\nM=M-1\nA=M\nD=M\n";
+  };
+
+  std::string pushDToStack()
+  {
+    return "@SP\nM=M+1\nA=M-1\nM=D\n";
+  }
+
+  std::string popSegToD(int idx, std::string segment)
+  {
+    if (segment == "this")
+    {
+      return "\n@" + std::to_string(idx) + "\nD=A\n@" +
+             memNameTable["pointer"] + "\nD=D+M\n";
+    }
+    else if (segment == "that")
+    {
+      return "\n@" + std::to_string(idx) + "\nD=A\n@" +
+             memNameTable["pointer_that"] + "\nD=D+M\n";
+    }
+    else if (segment == "static")
+    {
+      return "\n@" + std::to_string(idx) + "\nD=A\n@" +
+             memNameTable[segment] + "\nD=D+A\n";
+    }
+
+    return "\n@" + std::to_string(idx) + "\nD=A\n@" +
+           memNameTable[segment] + "\nD=D+M\n";
+  }
+
+  std::string popStaticPointerToD(int idx, std::string segment)
+  {
+    return "\n@" + std::to_string(idx) + "\nD=A\n@" +
+           memNameTable[segment] + "\nD=D+A\n";
+  }
+
+  std::string decrementStackPointer()
+  {
+    return "\n@SP\nM=M-1\n";
+  }
+
+  std::string incrementStackPointer()
+  {
+    return "\n@SP\nM=M+1\n";
+  }
+
+  std::string setAToStack()
+  {
+    return "\n@SP\nA=M\n";
+  }
+
+  void close()
+  {
+    myfile << "\n(INFINITE_LOOP)\n@INFINITE_LOOP\n0;JMP\n";
+    myfile.close();
+  }
+
+  void initTables()
+  {
+    memNameTable["local"] = "LCL";
+    memNameTable["argument"] = "ARG";
+    memNameTable["this"] = "THIS";
+    memNameTable["that"] = "THAT";
+    memNameTable["pointer"] = "3";
+    memNameTable["pointer_that"] = "4";
+    memNameTable["temp"] = "5";
+    memNameTable["static"] = "16";
+  }
+};
+
+#endif
+
+/*
+
+add
+---------------
+@SP 
+A=M-1
+D=M     //load y into D
+A=A-1   //address into x
+D=D+M   //save x+y into D
+@SP
+M=M-1   //SP--
+A=M-1   //access location below SP (ie. where x is)
+M=D     //save into x the x+y stored in D
+
+==============================================
+push constant i
+---------------
+@i
+D=A
+@SP
+M=M+1
+A=M-1
+M=D
+
+pop temp i
+---------------
+@i
+D=A
+@segment
+D=D+A 
+
+@temp
+M=D
+
+@SP
+M=M-1
+D=M
+
+@temp
+A=M
+M=D
+
+push segment i
+---------------
+@i
+D=A
+@segment
+D=D+A 
+A=D
+D=M
+
+@SP
+A=M
+M=D
+
+@SP
+M=M+1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+*/
---
+++ 08/VM_translator/main.cpp
@@ -0,0 +1,43 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <bitset>
+#include <cstring>
+
+#include "Parser.h"
+#include "CodeWriter.h"
+
+int main(int argc, char *argv[])
+{
+  std::string outputString = "";
+  Parser myParser(*(argv + 1));
+  CodeWriter myWriter(*(argv + 2));
+
+  myParser.showTokens();
+  int i = 1;
+  std::cout << "===============================" << std::endl;
+
+  while (myParser.hasMoreCommands())
+  {
+    std::cout << "Line: \t" << i << std::endl;
+    std::cout << "C Typ: \t" << myParser.commandType() << std::endl;
+    std::cout << "Arg 1: \t" << myParser.arg1() << std::endl;
+    std::cout << "Arg 2: \t" << myParser.arg2() << std::endl;
+    if (myParser.commandType() == "C_ARITHMETIC")
+    {
+      std::cout << "writeArith: \n"
+                << myWriter.writeArithmetic(myParser.arg1()) << std::endl;
+    }
+    else if (myParser.commandType() == "C_PUSH" || myParser.commandType() == "C_POP")
+    {
+      std::cout << "writePushPop: \n"
+                << myWriter.writePushPop(myParser.commandType(), myParser.arg1(), std::stoi(myParser.arg2())) << std::endl;
+    };
+    std::cout << "===============================" << std::endl;
+
+    i++;
+    myParser.advance();
+  };
+  myWriter.close();
+};
